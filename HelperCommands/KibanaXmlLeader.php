1fc05a4fc395adc38bec9fce1b6e782f195e2be0
davelai <dave.lai@kkyday.com>
2024-06-09 15:28:52 +0800

    add helper func

--- HelperCommands/KibanaXmlLeader.php
+++ HelperCommands/KibanaXmlLeader.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\v1\B2bOrderController;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
 function kibanaXmlToString()
 {
     $failCount = 0;
@@ -34,4 +38,50 @@ function kibanaXmlToString()
     }
 
     $this->line('fail count: ' . $failCount);
+}
+
+function shijiToApi()
+{
+    $storeCsv = base_path('app/Console/Commands/__temp/shiji_generate.csv');
+    if (($handle = fopen($storeCsv, 'r')) !== false) {
+        while (($row = fgetcsv($handle, 10000000, ',')) !== false) {
+            $client = new Client();
+
+            // 發送 GET 請求到自己的 API 路由
+            // content type: xml
+            $response = $client->post('http://0.0.0.0:8000/api/v1/b2bCallback/Shiji/aoeuaeou', [
+                'headers' => [
+                    'Content-Type' => 'application/xml',
+                ],
+                'body' => $row[0],
+            ]);
+
+            dd($response->getBody()->getContents(), 99);
+        }
+        fclose($handle);
+    }
+}
+
+
+function shijiToController()
+{
+    $storeCsv = base_path('app/Console/Commands/__temp/shiji_generate.csv');
+    $b2bOrderController = app(B2bOrderController::class);
+
+    if (($handle = fopen($storeCsv, 'r')) !== false) {
+        while (($row = fgetcsv($handle, 10000000, ',')) !== false) {
+            // 建立一個新的 Request 對象
+            $request = Request::create('/api/v1/b2bCallback/Shiji', 'POST', [], [], [], [], $row[0]);
+
+            // 設置 headers
+            $request->headers->set('Content-Type', 'application/xml');
+            $request->service = 'Shiji';
+
+            // 呼叫 B2bOrderController 的 callback 方法
+            $response = $b2bOrderController->callback($request, 'Shiji', 'aoeuaeou');
+
+            dd($response);
+        }
+        fclose($handle);
+    }
 }
